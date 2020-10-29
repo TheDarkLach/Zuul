e64f653bddbf5fb3db0586c595794ed519a56d1d
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-28 19:20:35 -0700

    items

---
+++ item.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include "item.h"
+
+using namespace std;
+
+Item::Item()
+{
+
+}
+
+//getters
+char* Item::getName()
+{
+  return name;
+}
+
+int Item::getId()
+{
+  return id;
+}
+
+void Item::setName(char* newName)
+{
+  name = newName;
+}
+
+void Item::setId(int newId)
+{
+  id = newId;
+}

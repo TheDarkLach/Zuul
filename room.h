f6f6a1cd2e7c8c93ec4032dcdb7792742fbd8d9a
TheDarkLach <11thunderstorm11@gmail.com>
2020-10-28 19:32:38 -0700

    room.h

---
+++ room.h
@@ -0,0 +1,34 @@
+
+#ifndef ROOM_H
+#define ROOM_H
+
+#include <iostream>
+#include <cstring>
+#include <map>
+
+using namespace std;
+
+class Room
+{
+
+ public:
+  Room();
+  //getters
+  char* getDescription();
+  int getId();
+  int getItem();
+  map<int, char*>* getExits();
+
+  //setters
+  void setDescription(char*);
+  void setExits(map<int, char*>);
+  void setId(int);
+  void setItem(int);
+
+ private:
+  char* description;
+  map<int, char*> exits;
+  int id;
+  int item;
+};
+#endif
